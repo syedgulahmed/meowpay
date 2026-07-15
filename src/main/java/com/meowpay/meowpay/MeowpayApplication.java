014f958baeb0b4cc41c6122a1b0c9861ff40de12
Syed Gul Ahmed <syedgul.cs17@iba-suk.edu.pk>
2026-07-16 04:09:41 +0500

    Fisrt commit, setting up project

---
+++ src/main/java/com/meowpay/meowpay/MeowpayApplication.java
@@ -0,0 +1,13 @@
+package com.meowpay.meowpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeowpayApplication.class, args);
+	}
+
+}
