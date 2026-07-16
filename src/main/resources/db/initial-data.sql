0de3152f80f90636fe115cb1661c8b89248752d2
Syed Gul Ahmed <syedgul.cs17@iba-suk.edu.pk>
2026-07-17 00:39:26 +0500

    Added cat accounts in DB

---
+++ src/main/resources/db/initial-data.sql
@@ -0,0 +1,5 @@
+insert into cat (name, treat) values ('Mochi', 120);
+insert into cat (name, treat) values ('Pepper', 95);
+insert into cat (name, treat) values ('Biscuit', 40);
+insert into cat (name, treat) values ('Waffles', 200);
+insert into cat (name, treat) values ('Luna', 60);
